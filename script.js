867f231634d1c3d210bd27e9be9b24ea74e4807c
Erick-Pavani <erickspavanii@gmail.com>
2022-08-04 22:14:50 -0300

    New header burguer menu implemented

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+mobile = document.getElementById('menu');
+
 function setMenu(e) {
     e.classList.toggle('on');
+    menu = document.getElementsByClassName('mobileMenu');
+    menu[0].classList.toggle('appear');
 }
+
+document.addEventListener('click', function(event) {
+    if (mobile.classList.contains('on') && !mobile.contains(event.target)) {
+        setMenu(mobile);
+    }
+});
+
+
