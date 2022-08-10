76db18fbae1b7d2348538f973c7b851347a2ec22
Erick-Pavani <erickspavanii@gmail.com>
2022-08-10 17:32:04 -0300

    Added a new page-switcher and page slide animation

--- js/page-switcher.js
+++ js/page-switcher.js
@@ -1,15 +1,63 @@
-window.addEventListener("load", () => {
-    setActivePage("Home-Page");
-});
+if (window.innerWidth <= 500 ) {
+    var nav = document.querySelector(".mobileMenu"),
+        navList = nav.querySelectorAll("li"),
+        totalNavList = navList.length;
+} else {
+    var nav = document.querySelector(".nav"),
+        navList = nav.querySelectorAll("li"),
+        totalNavList = navList.length;
+}
+const allSections = document.querySelectorAll(".section"),
+    totalSections = allSections.length;
 
-const alternatePages = document.querySelectorAll(".alternate-page");
-
-function setActivePage(name) {
-    alternatePages.forEach((page) => {
-        if (name === page.getAttribute("title")) {
-            page.removeAttribute("disabled");
-        } else {
-            page.setAttribute("disabled", "true");    
+for (let i = 0; i < totalNavList; i++) {
+    removeBackSection();
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        for (let i = 0; i < totalSections; i++)
+        {
+            allSections[i].classList.remove("back-section");
         }
-    });
+        for (let j = 0; j < totalNavList; j++)
+        {
+            if (navList[j].querySelector("a").classList.contains("active"))
+            {
+                addBackSection(j);    
+            }
+            navList[j].querySelector("a").classList.remove("active");    
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+}
+function addBackSection(num)
+{
+    allSections[num].classList.add("back-section");
+}
+function removeBackSection()
+{
+    for (let i = 0; i < totalSections; i++) {
+        allSections[i].classList.remove("back-section");
+    }
+}
+function showSection(element)
+{
+    for (let i = 0; i < totalSections; i++)
+    {
+        allSections[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
+function updateNav(element)
+{
+    for (let i = 0; i < totalNavList; i++)
+    {
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+        {
+            navList[i].querySelector("a").classList.add("active");   
+        }
+    }
 }
