9c2dc8fbe1fb77d12a0c2663d91d3e3c67ce58cb
Erick-Pavani <erickspavanii@gmail.com>
2022-08-14 13:07:23 -0300

    Now its impossible to send emoty emails

--- js/EmailButton.js
+++ js/EmailButton.js
@@ -1,14 +1,21 @@
 function SendMail() {
-    var params = {
-        subject : document.getElementById("mailSubject").value,
-        from_name : document.getElementById("mailName").value,
-        email_id : document.getElementById("mailEmail").value,
-        message : document.getElementById("mailMessage").value
+    if (document.getElementById("mailSubject").value && document.getElementById("mailName").value && document.getElementById("mailEmail").value && document.getElementById("mailMessage").value) {
+        var params = {
+            subject : document.getElementById("mailSubject").value,
+            from_name : document.getElementById("mailName").value,
+            email_id : document.getElementById("mailEmail").value,
+            message : document.getElementById("mailMessage").value
+        }
+
+        emailjs.send("service_mrdgala","template_5a1i30m", params).then(function (res) {
+            document.getElementById("mailSubject").value = "";
+            document.getElementById("mailName").value = "";
+            document.getElementById("mailEmail").value = "";
+            document.getElementById("mailMessage").value = "";
+        });
+
+        params = "";
+    } else {
+        alert("Preencha todos os campos para enviar o email!");
     }
-    emailjs.send("service_mrdgala","template_5a1i30m", params).then(function (res) {
-        document.getElementById("mailSubject").value = "";
-        document.getElementById("mailName").value = "";
-        document.getElementById("mailEmail").value = "";
-        document.getElementById("mailMessage").value = "";
-    });
 }
