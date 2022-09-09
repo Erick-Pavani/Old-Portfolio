44d54cbc8ec8b460adcd15b3d2eb361f62eb9d9f
Erick-Pavani <erickspavanii@gmail.com>
2022-09-09 16:24:10 -0300

    Testing IOS animations

--- js/projetos.js
+++ js/projetos.js
@@ -1,26 +1,23 @@
 if (window.innerWidth <= 500 ) {
-    var projects_counter = 0;
-    var projects_loop_counter = 0;
+    document.querySelectorAll(".projects_box a").style.pointerEvents = "none";
     const projects = document.querySelectorAll(".projects_box");
     projects.forEach(element => {
         element.addEventListener("click", function () {
-            projects_counter++;
-            const links = element.querySelectorAll("a");
+            var links = element.querySelectorAll("a");
             links.forEach(element => {
                 element.style.pointerEvents = "auto";
             })
         });    
     });
     // window.addEventListener("click", e => {
-    //     let handle
-    //     if (e.target.matches(".handle")) {
-    //         handle = e.target
-    //     } else {
-    //         handle = e.target.closest(".handle")
-    //     }
-    //     if (handle != null) {
-    //         onHandleClick(handle)
-    //     }
+    //     links.forEach(element => {
+    //         if (e != element) {
+    //             const links = element.querySelectorAll("a");
+    //             links.forEach(link => {
+    //                 link.style.pointerEvents = "none";
+    //             })
+    //         }
+    //     });
     // })
 }
 
