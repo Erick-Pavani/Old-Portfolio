d67189a9704ae3dd1a096dffaa7639f4ca10c3a9
Erick-Pavani <erickspavanii@gmail.com>
2022-09-09 21:09:45 -0300

    Testing -webkit-mas-image on projects page for IOS

--- js/projetos.js
+++ js/projetos.js
@@ -1,23 +1,33 @@
-if (window.innerWidth <= 500 ) {
-    document.querySelectorAll(".projects_box a").style.pointerEvents = "none";
-    const projects = document.querySelectorAll(".projects_box");
-    projects.forEach(element => {
-        element.addEventListener("click", function () {
-            var links = element.querySelectorAll("a");
-            links.forEach(element => {
-                element.style.pointerEvents = "auto";
-            })
-        });    
-    });
-    // window.addEventListener("click", e => {
-    //     links.forEach(element => {
-    //         if (e != element) {
-    //             const links = element.querySelectorAll("a");
-    //             links.forEach(link => {
-    //                 link.style.pointerEvents = "none";
-    //             })
-    //         }
-    //     });
-    // })
-}
+// if (window.innerWidth <= 500 ) {
+    
+//     const start = document.querySelectorAll(".projects_box a");
+//     start.forEach(link => {
+//         link.style.pointerEvents = "none";
+//     });
+    
+//     // const projects = document.querySelectorAll(".projects_box");
+//     // projects.forEach(element => {
+//     //     element.addEventListener("click", function () {
+//     //         const links = element.querySelectorAll("a");
+//     //         links.forEach(element => {
+//     //             element.style.pointerEvents = "auto";
+//     //         })
+//     //     });    
+//     // });
+
+//     window.addEventListener("click", e => {
+//         const links = document.querySelectorAll(".projects_box a");
+//         console.log()
+//         links.forEach(element => {
+//             // if (e !== element.parentNode && !element.parentNode.contains(e)) {
+//             if (e !== element.parentNode) {
+//                 console.log("teste")
+//                 element.style.pointerEvents = "none";
+//             } else {
+//                 element.style.pointerEvents = "auto";
+//                 console.log("Clicou no parent node")
+//             }
+//         });
+//     })
+// }
 
