26c316cb29a804eb120eb01baafadd91cd16cb99
Erick-Pavani <erickspavanii@gmail.com>
2022-09-09 22:09:44 -0300

    Fixed projects links and hover effects

--- js/projetos.js
+++ js/projetos.js
@@ -1,33 +1,21 @@
-// if (window.innerWidth <= 500 ) {
+if (window.innerWidth <= 500 ) {
     
-//     const start = document.querySelectorAll(".projects_box a");
-//     start.forEach(link => {
-//         link.style.pointerEvents = "none";
-//     });
+    const projetos = document.querySelectorAll(".projects_box");
     
-//     // const projects = document.querySelectorAll(".projects_box");
-//     // projects.forEach(element => {
-//     //     element.addEventListener("click", function () {
-//     //         const links = element.querySelectorAll("a");
-//     //         links.forEach(element => {
-//     //             element.style.pointerEvents = "auto";
-//     //         })
-//     //     });    
-//     // });
-
-//     window.addEventListener("click", e => {
-//         const links = document.querySelectorAll(".projects_box a");
-//         console.log()
-//         links.forEach(element => {
-//             // if (e !== element.parentNode && !element.parentNode.contains(e)) {
-//             if (e !== element.parentNode) {
-//                 console.log("teste")
-//                 element.style.pointerEvents = "none";
-//             } else {
-//                 element.style.pointerEvents = "auto";
-//                 console.log("Clicou no parent node")
-//             }
-//         });
-//     })
-// }
-
+    projetos.forEach(projeto => {
+        
+        projeto.addEventListener("click", function() {
+            projeto.querySelectorAll("a").forEach(link => {
+                setTimeout(function () {
+                    link.style.pointerEvents = "auto";
+                }, 500)
+            })
+        })
+        
+        projeto.addEventListener("mouseleave", function() {
+            projeto.querySelectorAll("a").forEach(link => {
+                link.style.pointerEvents = "none";
+            })
+        })
+    });
+}
